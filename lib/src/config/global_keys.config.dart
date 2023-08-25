c61be6fbd3f9ea9f6ffc7c9e0346b301cd83c947
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-25 22:06:26 +0530

    🛠  REFACTOR: globalkeys config (router config -1)

---
+++ lib/src/config/global_keys.config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GlobalKeysConfig {
+  static GlobalKey<NavigatorState> main = GlobalKey();
+  static GlobalKey<NavigatorState> navBar = GlobalKey();
+  static GlobalKey<NavigatorState> mainScaffold = GlobalKey();
+}
