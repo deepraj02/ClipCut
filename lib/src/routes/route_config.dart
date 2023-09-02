2ec9ed9a342498dc984a59ef4c11eb452d945cd0
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-09-03 01:35:23 +0530

    📦 FEAT: navigation configired for the project

--- lib/src/routes/route_config.dart
+++ lib/src/routes/route_config.dart
@@ -1,19 +1,34 @@
-import 'package:clipcut/src/config/global_keys.config.dart';
+import '../config/global_keys.config.dart';
+import '../features/shell/shell.page.dart';
 import 'package:go_router/go_router.dart';
 
+import '../features/home/presentation/pages/home.page.dart';
 import '../features/splash/presentation/pages/splash.page.dart';
 
 class AppRoutes {
-
   static final router = GoRouter(
-    initialLocation: SplashPage.route,
-    navigatorKey: GlobalKeysConfig.main,
-    routes: [
-      GoRoute(
-        parentNavigatorKey: GlobalKeysConfig.main,
-        path: SplashPage.route,
-        builder: (context, state) => const SplashPage(),
-      ),
-    ]
-  );
+    debugLogDiagnostics: true,
+      initialLocation: SplashPage.route,
+      navigatorKey: GlobalKeysConfig.main,
+      routes: [
+        GoRoute(
+          parentNavigatorKey: GlobalKeysConfig.main,
+          path: SplashPage.route,
+          builder: (context, state) => const SplashPage(),
+        ),
+        ShellRoute(
+          navigatorKey: GlobalKeysConfig.navBar,
+          builder: (context, state, child) {
+            return ShellPage(child: child);
+          },
+          routes: [
+            GoRoute(
+                parentNavigatorKey: GlobalKeysConfig.navBar,
+                path: HomePage.route,
+                pageBuilder: (context, state) {
+                  return const NoTransitionPage(child: HomePage());
+                }),
+          ],
+        ),
+      ]);
 }
