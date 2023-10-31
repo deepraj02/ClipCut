7ecf0248a8c5f00539190a9e9be485e7e406d2e7
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-10-31 15:55:01 +0530

    🎨 STYLE: homepage layout design --ongoing

--- lib/src/features/home/presentation/pages/home.page.dart
+++ lib/src/features/home/presentation/pages/home.page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../../config/global_keys.config.dart';
+
 class HomePage extends StatelessWidget {
   static const String route = '/home';
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(' Hellllo!'),
+    return Center(
+      child: IconButton.filled(
+        onPressed: () {
+          debugPrint(GlobalKeysConfig.navBar.currentContext.toString());
+        },
+        icon: const Icon(Icons.upload_file_outlined),
+      ),
     );
   }
 }
