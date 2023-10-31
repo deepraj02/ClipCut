b7ff797108516f043d23bfd55ebf7a30ce7ba373
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-10-31 18:40:25 +0530

    📦 FEAT: image data routing from home to editing page setup

--- lib/src/features/home/presentation/pages/home.page.dart
+++ lib/src/features/home/presentation/pages/home.page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+import '../../../editing/presentation/pages/editing.page.dart';
+
 class HomePage extends StatelessWidget {
   static const String route = '/home';
   const HomePage({super.key});
@@ -13,7 +15,16 @@ class HomePage extends StatelessWidget {
           XFile? file = await ImagePicker().pickImage(
             source: ImageSource.gallery,
           );
-          if (file != null) {}
+          if (file != null) {
+            //GoRouter.of(GlobalKeysConfig.navBar.currentContext!)
+            // context.goNamed(EditingPage.route, queryParameters: {
+            //   'selectedImage': file.path,
+            // });
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                  builder: (context) => EditingPage(selectedImage: file.path)),
+            );
+          }
         },
         icon: const Icon(Icons.upload_file_outlined),
       ),
