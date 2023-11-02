05f554f7c23ef1924d8394907cd6bc840be2c452
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-11-02 22:40:21 +0530

    🛠  REFACTOR: appbar navigation btw pages

--- lib/src/features/shell/shell.page.dart
+++ lib/src/features/shell/shell.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../config/global_keys.config.dart';
 
@@ -10,12 +11,21 @@ class ShellPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(GlobalKeysConfig.main.currentContext.toString())),
+        appBar: AppBar(
+          title: Text(GlobalKeysConfig.main.currentContext.toString()),
+          leading: IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(Icons.arrow_back_ios)),
+        ),
         key: GlobalKeysConfig.mainScaffold,
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         body: Stack(
           children: [
-            Center(child: child),
+            Center(
+              child: child,
+            ),
           ],
         ));
   }
