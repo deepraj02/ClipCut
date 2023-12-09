f08cc97342d7ba160bd141a53741e1477e1337ec
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-12-09 19:03:26 +0530

    📦 FEAT: Text dialog (text input)

--- lib/src/features/editing/presentation/widgets/floating.widget.dart
+++ lib/src/features/editing/presentation/widgets/floating.widget.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
-import '../../../../../styles/colors.dart';
-
-Widget get textAddButton => FloatingActionButton(
-      onPressed: () {},
-      backgroundColor: ClipCutColors.darkTertiary,
-      tooltip: 'Add Text',
-      child: const Icon(
-        Icons.edit,
-        color: ClipCutColors.lightPrimary,
+addNewDialog(context, TextEditingController controller) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: const Text(
+        'Add New Text',
       ),
-    );
+      content: TextField(
+        controller: controller,
+        maxLines: 5,
+        decoration: const InputDecoration(
+          suffixIcon: Icon(
+            Icons.edit,
+          ),
+          filled: true,
+          hintText: 'Your Text Here',
+        ),
+      ),
+    ),
+  );
+}
